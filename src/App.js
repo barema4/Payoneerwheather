3ffd5190ad0ffdaf450f2372afc8495cf9a0eef7
@sam.rubarema <sam.rubarema@bposeats.com>
2021-05-03 22:23:55 +0200

    remove react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import {HashRouter as Router, Route} from 'react-router-dom'
+// import {HashRouter as Router, Route} from 'react-router-dom'
 import './App.css';
 import WeatherForecast from './components/WeatherForecast'
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Route exact path="/" component={WeatherForecast} />
-    </Router>
+      <WeatherForecast />
     </div>
   );
 }
